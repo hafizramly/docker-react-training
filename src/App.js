b0e0ff7b6a4391e3ad73e9caf74a59d201a2ddd2
Hafiz <hafiz.ramly@gmail.com>
2019-02-13 08:30:30 +0800

    Add DEV branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello web <code>test</code> user !
+            New branch <code>dev</code> created !
           </p>
           <a
             className="App-link"
